c86e00f71903071b544c8994ea289967fa5750cf
GNtem2 <thanh.phan@monash.edu>
2020-04-17 09:45:58 +1000

    PRISMA

---
+++ Prisma_flow_chart.R
@@ -0,0 +1,12 @@
+library(PRISMAstatement)
+#example from Spot sign paper. Stroke 2019
+prisma(found = 193,
+       found_other = 27,
+       no_dupes = 141, 
+       screened = 141, 
+       screen_exclusions = 3, 
+       full_text = 138,
+       full_text_exclusions = 112, 
+       qualitative = 26, 
+       quantitative = 26,
+       width = 800, height = 800)
