9260bc418351c607aef77862fe8e06cdbe5eacac
GNtem2 <thanh.phan@monash.edu>
2020-07-23 14:36:20 +1000

    DiagrammeR

--- Prisma_flow_chart.R
+++ Prisma_flow_chart.R
@@ -10,3 +10,41 @@ prisma(found = 193,
        qualitative = 26, 
        quantitative = 26,
        width = 800, height = 800)
+
+#https://rich-iannone.github.io/DiagrammeR/graphviz_and_mermaid.html#attributes
+library(DiagrammeR)
+grViz("
+digraph boxes_and_circles {
+
+  # a 'graph' statement
+  graph [overlap = true, fontsize = 10]
+
+  # several 'node' statements
+  node [shape = box,
+        fontname = Helvetica]
+  Stroke
+
+  node [shape = oval,
+        fixedsize = false,
+        color=red,
+        width = 0.9] 
+  Hypertension; 'No Hypertension'
+  
+  node [shape= circle,
+  fontcolor=red,
+        color=blue,
+        fixedsize=false]
+
+  Hypokalemia; 'No Hypokalemia'        
+
+  # several 'edge' statements
+  edge [arrowhead=diamond]
+  
+  Stroke->{Hypertension, 'No Hypertension'}
+  Hypertension->{Hypokalemia, 'No Hypokalemia'}
+  
+}
+
+
+
+")
